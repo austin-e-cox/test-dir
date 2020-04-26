817931a75f305c0299bd98cf144c25bb5f44e27d
Austin Cox <austin.ed.cox@gmail.com>
2020-04-26 01:15:39 -0700

    Updates

---
+++ precache-manifest.1823fdd9bd63ceca1ed84805b221e6f6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1d892743c43a66556e7bacd27efbd8f",
+    "url": "/test-dir/index.html"
+  },
+  {
+    "revision": "e9165c0c5e2d9da10c2f",
+    "url": "/test-dir/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "020edd6da4daa24f31a8",
+    "url": "/test-dir/static/js/2.21f73562.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test-dir/static/js/2.21f73562.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9165c0c5e2d9da10c2f",
+    "url": "/test-dir/static/js/main.ea0396ac.chunk.js"
+  },
+  {
+    "revision": "ba57bd8274c74e26b605",
+    "url": "/test-dir/static/js/runtime-main.d4eeb92d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test-dir/static/media/logo.5d5d9eef.svg"
+  }
+]);
